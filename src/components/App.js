b5af20cb390e7d12fb7c617293fbaaf7d45cafce
SourabhPisal <80575233+SourabhPisal@users.noreply.github.com>
2022-07-08 12:43:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ const App = () => {
   
   return (
     <div id="main">
-
+       <h1 id="marco-polo">{flag ? "Marco" : "Polo"}</h1>
+        <button id="marco-polo-toggler" onClick={setFlag(!flag)}>{flag ? "Polo" : "Marco"}</button>
+        onClick={()=>{setFlag(!flag)}}
     </div>
   )
 }
